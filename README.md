7d9951c091a660b12cac9eeb533904f65f8f8256
Kate Johnson <johnson.kat.g@gmail.com>
2023-03-06 16:52:54 -0500

    Initial commit with User model

---
+++ README.md
@@ -0,0 +1,17 @@
+# FLASK SOCIAL NETWORK APP
+
+## Description
+
+## Installation
+
+1. Clone this repository
+2. Open terminal and install requirements with ```pip install -r requirements.txt```
+
+## Technologies Used
+
+- flask
+- flask-login
+- flask-bcrypt
+- peewee
+- datetime
+- Sqlite
---
+++ models.py
@@ -0,0 +1,32 @@
+import datetime
+
+from flask_bcrypt import generate_password_hash
+from flask_login import UserMixin
+from peewee import *
+
+
+DATABASE = SqliteDatabase('social.db')
+
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+    is_admin = BooleanField(default=False)
+
+    class Meta:
+        database = DATABASE
+        # view newest users first
+        order_by = ('-joined_at',)
+
+    # describes method that belongs to a class that can create the class it belongs to (cls = class), creates class instance before calling method
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            # use cls instead of self to not have to create user instance first
+            cls.create(username=username, email=email,
+                       password=generate_password_hash(password), is_admin=admin)
+        except IntegrityError:
+            # raise error if not unique values
+            raise ValueError('User already exists')
